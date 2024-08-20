2cb075f4b45a19c5daf4b7b51b537b905e7f24f7
Steve0Greatness <steve0greatnessiscool@gmail.com>
2024-08-19 23:56:32 -0700

    License a lot of stuff under BSD 3-Clause
    Basically all code in this repo is under that license now.
    
    Since nothing uses the GPL v3 anymore, the copy of the license has been
    removed.

--- MiscUtils.py
+++ MiscUtils.py
@@ -1,20 +1,14 @@
 """
-    This is a small static site generator written to generate the Nekoweb Wiki: <https://wiki.nekoweb.org>.
-    Copyright (C) 2024  Steve0Greatness
+Copyright 2024 Nekoweb Wiki
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This software is licensed under the 3-clause BSD license.
+You should have recieved a copy of such license with this software,
+if you did not, a copy can be found at the following
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    https://opensource.org/license/BSD-3-Clause
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
+Otherwise, a copy of this license can be found in the root directory
+of this project."""
 
 from os.path import exists as PathExists, join as JoinPath
 from shutil import rmtree as RMRF 
--- README.md
+++ README.md
@@ -47,9 +47,10 @@ bear with me.
 
 ## Licensing
 
-* `main.py`, `RenderUtils.py` and `MiscUtils.py` all use the GPL v3-or-later. A copy of this license can be
-  found at `/GPLv3.txt` in the source repository.
+* `main.py`, `RenderUtils.py` and `MiscUtils.py` all use the BSD 3-Clause license. A copy of this license can be
+  found at `/BSD-3-CLAUSE.txt` in the source repository.
 * `config.py` is under the Unlicense. A copy of this license can be found within the file itself.
 * Wiki content (under `content` and `images`) is licensed under CC BY-SA. A copy is included at `/CC_BY-SA.txt` on the
   site, and at `/static/CC_BY-SA.txt` in the source repository.
-* Views are not currently licensed, this hasn't been discussed yet, but should be.
+* Views are licensed under the BSD 3-Clause license. A copy of this license can be found at `/BSD-3-CLAUSE.txt`.
+* Anything under `static` should be assumed to be unlicensed unless specified otherwise.
