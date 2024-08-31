294fbba138feef8a938ff65c92f897ba9fdea638
Steve0Greatness <steve0greatnessiscool@gmail.com>
2024-08-30 23:45:11 -0700

    Various changes to license ads
    1. Relicense config under same license as the rest of the codebase:
       - It's easier.
       - Although it was committed to the public domain, the public domain
         allows for relicensing.
       - The unlicense is controversial.
    2. Have branding's README point to the global CC_BY-SA.txt copy.
    3. Update README to reflect these changes.

--- branding/README.md
+++ branding/README.md
@@ -1,6 +1,6 @@
 # `/branding` directory
 
-This directory holds source and compiled copies of all wiki logos, icons, and general other branding related to the wiki. All content in licensed under Creative Commons by ShareAlike International 4.0; a copy of this license is available in this directory: `CC_BY-SA.txt`.
+This directory holds source and compiled copies of all wiki logos, icons, and general other branding related to the wiki. All content in licensed under Creative Commons by ShareAlike International 4.0; a copy of this license is available at`/CC_BY-SA.txt` in this repository.
 
 ## `.buildinclude`
 
--- config.py
+++ config.py
@@ -1,28 +1,14 @@
 """
-This is free and unencumbered software released into the public domain.
+Copyright 2024 Nekoweb Wiki
 
-Anyone is free to copy, modify, publish, use, compile, sell, or
-distribute this software, either in source code form or as a compiled
-binary, for any purpose, commercial or non-commercial, and by any
-means.
+This software is licensed under the 3-clause BSD license.
+You should have recieved a copy of such license with this software,
+if you did not, a copy can be found at the following
 
-In jurisdictions that recognize copyright laws, the author or authors
-of this software dedicate any and all copyright interest in the
-software to the public domain. We make this dedication for the benefit
-of the public at large and to the detriment of our heirs and
-successors. We intend this dedication to be an overt act of
-relinquishment in perpetuity of all present and future rights to this
-software under copyright law.
+    https://opensource.org/license/BSD-3-Clause
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
-For more information, please refer to <http://unlicense.org/>
+Otherwise, a copy of this license can be found in the root directory
+of this project.
 """
 
 from re import compile as ReCompile, X as ReX
