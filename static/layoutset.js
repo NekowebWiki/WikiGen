d088f8716e545a2b2bc7a38532657fcd876a95e6
Steve0Greatness <steve0greatnessiscool@gmail.com>
2024-10-06 18:22:13 -0700

    Add license header to layout scripts

--- static/layoutset.js
+++ static/layoutset.js
@@ -1,3 +1,17 @@
+/*
+Copyright 2024 Nekoweb Wiki
+
+This software is licensed under the 3-clause BSD license.
+You should have recieved a copy of such license with this software,
+if you did not, a copy can be found at the following
+
+    https://opensource.org/license/BSD-3-Clause
+
+Otherwise, a copy of the license can be found in the root directory
+of the source files, see
+
+    https://github.com/NekowebWiki/WikiGen
+*/
 const settings = document.querySelector("#layout-form");
 
 function ValueOrLocalStorage(wanted, name, value) {
